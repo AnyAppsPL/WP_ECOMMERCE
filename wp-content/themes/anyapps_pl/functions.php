e004004f2a536a9a588b34531a8fa596607d4da1
Rafał Jaworski <anygroup@icloud.com>
2020-02-07 00:14:38 +0100

    instal fastest cache and new location for overriden functions

--- wp-content/themes/anyapps_pl/functions.php
+++ wp-content/themes/anyapps_pl/functions.php
@@ -1,4 +1,15 @@
 <?php
 
+require_once "override-functions/storefront_functions.php";
+require_once "override-functions/woocommerce_functions.php";
+
 define('THEME_PATH',get_stylesheet_directory_uri());
 define('ROOT_PATH', get_home_url());
+
+//Add and remove parts of website
+add_action('init', function (){
+
+});
+
+
+
